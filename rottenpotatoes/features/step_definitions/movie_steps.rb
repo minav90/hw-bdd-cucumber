99fbd455026e890f36eefc6d75979234b046a835
Minav <you@example.com>
2015-09-23 10:58:38 -0500

    final2 commit

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -3,10 +3,7 @@
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     Movie.create(movie)
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
   end
-   #fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -21,9 +18,6 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
                end
        end
        its_sorted.should be true
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  #fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -60,6 +54,4 @@ end
 
 Then /I should see all the movies/ do
     Movie.count(:all).should == 10
-  # Make sure that all the movies in the app are visible in the table
-  #fail "Unimplemented"
 end
